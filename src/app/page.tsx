a5b9d9446b3abc7d7da02ffd294cf7bf5d72b6ee
KhaledAlorayir <jnonkh@icloud.com>
2025-10-02 14:48:44 +0300

    layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import {ChatMessages} from "@/components/chat-messages";
 export default function Home() {
     const {messages, sendMessage, status} = useChat();
     return (
-        <div className="max-w-7xl mx-auto pt-12 py-4 px-6 relative size-full h-screen">
+        <div className="max-w-7xl mx-auto pt-12 py-4 px-2 md:px-6 relative size-full h-screen">
             <div className="flex flex-col h-full gap-4">
                 <ChatMessages messages={messages} status={status}/>
                 <ChatInput onSubmit={(value) => sendMessage({text: value})}/>
--- src/components/chat-messages.tsx
+++ src/components/chat-messages.tsx
@@ -1,12 +1,11 @@
 import {ChatStatus, UIMessage} from "ai";
 import {Conversation, ConversationContent, ConversationScrollButton,} from "@/components/ai-elements/conversation";
-import {Source, Sources, SourcesContent, SourcesTrigger} from "@/components/ai-elements/sources";
 import {Response} from "@/components/ai-elements/response";
 import {Fragment} from "react";
 import {Message, MessageContent} from "@/components/ai-elements/message";
 import {Action, Actions} from "@/components/ai-elements/actions";
-import {CopyIcon, Loader, RefreshCcwIcon} from "lucide-react";
-import {Reasoning, ReasoningContent, ReasoningTrigger} from "@/components/ai-elements/reasoning";
+import {CopyIcon} from "lucide-react";
+import {Loader} from "@/components/ai-elements/loader";
 
 interface Props {
     messages: UIMessage[];
@@ -19,77 +18,51 @@ export function ChatMessages(props: Props) {
             <ConversationContent>
                 {props.messages.map((message) => (
                     <div key={message.id}>
-                        {message.role === 'assistant' && message.parts.filter((part) => part.type === 'source-url').length > 0 && (
-                            <Sources>
-                                <SourcesTrigger
-                                    count={
-                                        message.parts.filter(
-                                            (part) => part.type === 'source-url',
-                                        ).length
-                                    }
-                                />
-                                {message.parts.filter((part) => part.type === 'source-url').map((part, i) => (
-                                    <SourcesContent key={`${message.id}-${i}`}>
-                                        <Source
-                                            key={`${message.id}-${i}`}
-                                            href={part.url}
-                                            title={part.url}
-                                        />
-                                    </SourcesContent>
-                                ))}
-                            </Sources>
-                        )}
-                        {message.parts.map((part, i) => {
-                            switch (part.type) {
-                                case 'text':
-                                    return (
-                                        <Fragment key={`${message.id}-${i}`}>
-                                            <Message from={message.role}>
-                                                <MessageContent>
-                                                    <Response>
-                                                        {part.text}
-                                                    </Response>
-                                                </MessageContent>
-                                            </Message>
-                                            {message.role === 'assistant' && i === props.messages.length - 1 && (
-                                                <Actions className="mt-2">
-                                                    <Action
-                                                        label="Retry"
-                                                    >
-                                                        <RefreshCcwIcon className="size-3" />
-                                                    </Action>
-                                                    <Action
-                                                        onClick={() =>
-                                                            navigator.clipboard.writeText(part.text)
-                                                        }
-                                                        label="Copy"
-                                                    >
-                                                        <CopyIcon className="size-3" />
-                                                    </Action>
-                                                </Actions>
-                                            )}
-                                        </Fragment>
-                                    );
-                                case 'reasoning':
-                                    return (
-                                        <Reasoning
-                                            key={`${message.id}-${i}`}
-                                            className="w-full"
-                                            isStreaming={props.status === 'streaming' && i === message.parts.length - 1 && message.id === props.messages.at(-1)?.id}
-                                        >
-                                            <ReasoningTrigger />
-                                            <ReasoningContent>{part.text}</ReasoningContent>
-                                        </Reasoning>
-                                    );
-                                default:
-                                    return null;
-                            }
-                        })}
+                        <MessageCard
+                            message={message}
+                        />
                     </div>
                 ))}
-                {props.status === 'submitted' && <Loader />}
+                {props.status === "submitted" && <Loader />}
             </ConversationContent>
-            <ConversationScrollButton />
+            <ConversationScrollButton/>
         </Conversation>
-    )
+    );
+}
+
+function MessageCard({message}: { message: UIMessage }) {
+    return (
+        <>
+            {message.parts.map((part, i) => {
+                switch (part.type) {
+                    case "text":
+                        return (
+                            <Fragment key={`${message.id}-${i}`}>
+                                <Message from={message.role}>
+                                    <MessageContent>
+                                        <Response>
+                                            {part.text}
+                                        </Response>
+                                    </MessageContent>
+                                </Message>
+                                {message.role === "assistant" && i === message.parts.length - 1 && (
+                                    <Actions>
+                                        <Action
+                                            onClick={() =>
+                                                navigator.clipboard.writeText(part.text)
+                                            }
+                                            label="Copy"
+                                        >
+                                            <CopyIcon className="size-4"/>
+                                        </Action>
+                                    </Actions>
+                                )}
+                            </Fragment>
+                        );
+                    default:
+                        return null;
+                }
+            })}
+        </>
+    );
 }
