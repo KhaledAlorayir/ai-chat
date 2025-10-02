a8407e49ae420b471cbe97e9382a8bbffa7fe15c
KhaledAlorayir <jnonkh@icloud.com>
2025-10-02 03:07:05 +0300

    init vercel ai sdk

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -4,9 +4,10 @@ const schema = z.object({
     TURSO_AUTH_TOKEN: z.string().trim(),
     TURSO_DATABASE_URL: z.string().trim(),
     BETTER_AUTH_SECRET: z.string().trim(),
-    BETTER_AUTH_URL: z.url(),
-    GOOGLE_CLIENT_ID: z.string(),
-    GOOGLE_CLIENT_SECRET: z.string()
+    BETTER_AUTH_URL: z.url().trim(),
+    GOOGLE_CLIENT_ID: z.string().trim(),
+    GOOGLE_CLIENT_SECRET: z.string().trim(),
+    OPENAI_API_KEY: z.string().trim()
 });
 
 
